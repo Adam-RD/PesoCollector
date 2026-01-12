594a75761eed0343dca955796d5bdb151b9777b0
Adam <adamdiaz1214@hotmail.com>
2026-01-11 22:22:35 -0400

    Uopded Icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ import "../styles/globals.css";
 export const metadata: Metadata = {
   title: "PesoCollector",
   description: "Panel para gestionar clientes, deudas y pagos",
+  icons: {
+    icon: "/PesoCollector.ico",
+    shortcut: "/PesoCollector.ico",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
